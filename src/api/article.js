57e97df3fff540dafb2cd94e970e7e2031b5edef
Edge <edge27452696803@gmail.com>
2024-07-23 02:26:52 +0800

    add

--- src/api/article.js
+++ src/api/article.js
@@ -1,3 +1,7 @@
 import request from '../utils/request'
 
 export const artGetChannelsService = () => request.get('/my/cate/list')
+
+export const artAddChannelService = (data) => request.post('/my/cate/add', data)
+
+export const artEditChannelService = (data) => request.put('/my/cate/info', data)
--- src/views/article/components/channelEdit.vue
+++ src/views/article/components/channelEdit.vue
@@ -1,10 +1,20 @@
 <script setup>
 import { ref } from 'vue'
 const dialogVisible = ref(false)
+const fromModel = ref({
+  cate_name: '',
+  cate_alias: ''
+})
+
+const rules = ref({
+  cate_name: [{ required: true, message: '请输入分类名称', trigger: 'blur' }],
+  cate_alias: [{ required: true, message: '请输入分类别名', trigger: 'blur' }]
+})
 
 const open = (row) => {
   console.log(row)
   dialogVisible.value = true
+  fromModel.value = row
 }
 
 defineExpose({
@@ -15,11 +25,19 @@ defineExpose({
 <template>
   <el-dialog
     v-model="dialogVisible"
-    title="编辑分类"
+    :title="fromModel.cate_name ? '编辑分类' : '新增分类'"
     width="30%"
     :before-close="handleClose"
+    class="dialog"
   >
-    <span>内容</span>
+    <el-form :model="fromModel" :rules="rules" class="form">
+      <el-form-item label="分类名称" prop="cate_name">
+        <el-input v-model="fromModel.cate_name" placeholder="请输入分类名称"></el-input>
+      </el-form-item>
+      <el-form-item label="分类别名" prop="cate_alias">
+        <el-input v-model="fromModel.cate_alias" placeholder="请输入分类别名"></el-input>
+      </el-form-item>
+    </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
@@ -28,3 +46,12 @@ defineExpose({
     </template>
   </el-dialog>
 </template>
+
+<style lang="scss" scoped>
+.dialog {
+  display: flex;
+  .form {
+    margin: 20px auto;
+  }
+}
+</style>
